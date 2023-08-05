0cf6da9f7a74e830a557acc4d1b31ee93835dfbd
Stanley Diki <stanleydiki@gmail.com>
2023-08-05 11:59:31 +0100

    adding portfolio project files

---
+++ src/components/data.js
@@ -0,0 +1,60 @@
+import React from "react";
+
+export const portfolio = [
+  {
+    name: "Stanley",
+
+    email: "atakistanley@gmil.com",
+
+    fullName: "Stanley Ataki",
+
+    paragraph: "",
+
+    photo: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "",
+    imgSrc: "",
+    url: "",
+  },
+  {
+    id: 2,
+    title: "",
+    imgSrc: "",
+    url: "",
+  },
+  {
+    id: 3,
+    title: "",
+    imgSrc: "",
+    url: "",
+  },
+  {
+    id: 4,
+    title: "",
+    imgSrc: "",
+    url: "",
+  },
+];
+
+export const social = [
+  {
+    name: "GitHub",
+    url: "",
+    icon: "",
+  },
+  {
+    name: "LinkedIn",
+    url: "",
+    icon: "",
+  },
+  {
+    name: "Twitter",
+    url: "",
+    icon: "",
+  },
+];
