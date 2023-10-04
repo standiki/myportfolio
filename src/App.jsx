a94485bda3c83b0bff35d382b5f45b9541861642
Stanley Diki <stanleydiki@gmail.com>
2023-10-04 13:33:31 +0100

    App Structure
    Changed the App.jsx structure to not be the centre for importing multiple components.
    Adding the structure for the Landing.jsx component to be the centre for housing multiple components imports.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import Header from "./components/Header";
-
-import { userInfo, projects, social } from "./components/data";
+import Landing from "./components/Landing";
 
 function App() {
   return (
     <>
-      <Header name={userInfo[0].name} />
+      <Landing />
     </>
   );
 }
---
+++ src/components/Landing.jsx
@@ -0,0 +1,9 @@
+import Header from "./Header";
+
+import data from "./data";
+
+const Landing = () => {
+  return <div></div>;
+};
+
+export default Landing;
